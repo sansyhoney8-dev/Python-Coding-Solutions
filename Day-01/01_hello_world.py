49ec2d0255634f7a372655299713b9f49f89d91d
Sanskriti kumari <sansyhoney8@gmail.com>
2026-02-18 01:29:10 +0530

    Create 01_hello_world.py

---
+++ Day-01/01_hello_world.py
@@ -0,0 +1,7 @@
+# Questions 01 : Welcome Message
+# Created by : Sanskriti
+name = "Sanskriti kumari"
+college_year = "3rd Year ( 6th Semester )"
+print("Hello! This is my first python code for TNP Placements.")
+print(f"Name :{name}")
+print(f"Current Status : {college_year}")
